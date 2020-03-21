ca049aec6f323074893a647f730e20e56f944576
jinggggwenn <e0323466@u.nus.edu>
2020-03-21 22:52:26 +0800

    completed keyup product search | edited sidemenu, headertab

--- tellybuddy/tellybuddy-war/src/java/jsf/validator/ReorderQuantityValidator.java
+++ tellybuddy/tellybuddy-war/src/java/jsf/validator/ReorderQuantityValidator.java
@@ -1,3 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.validator;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
@@ -6,11 +12,6 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author ngjin
